b694060dc3c165ad2995cf1481662ac2cb8629a0
aricallen <aric.allen2@gmail.com>
2021-08-24 20:00:56 -0700

    revamp cards

--- stories/atoms/Card.stories.tsx
+++ stories/atoms/Card.stories.tsx
@@ -1,35 +1,30 @@
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
-import { Card, CardProps } from '../../src';
-import { styled } from '../..';
+import { Card, CardProps, SimpleGrid } from '../../src';
 
 export default {
   title: 'Components/Atoms/Card',
   component: Card,
 } as Meta;
 
-const Template: Story<CardProps> = (args) => <Card {...args} />;
-
-export const WithHeading = Template.bind({});
-WithHeading.args = {
-  text: 'CardTitle',
-};
-
-const Wrapper = styled('div')`
-  display: grid;
-  grid-template-rows: 40% 40%;
-  grid-template-columns: 40% 40%;
-  grid-gap: 4em;
-`;
-
 const lorem =
   'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dignissimos ut voluptate eveniet? Sunt totam, quidem, nobis corrupti voluptatum aliquam expedita voluptas alias laborum eos dolor esse fugiat? Explicabo, tenetur.';
 
+const Template: Story<CardProps> = (args) => <Card {...args} />;
+
+export const Single = Template.bind({});
+Single.args = {
+  title: 'Card Title',
+  text: lorem,
+  imageSrc:
+    'https://res.cloudinary.com/saimano/image/upload/v1622036153/RaidGuild/icons/red/consultations_lr6ef4.png',
+};
+
 const images = [
-  "https://res.cloudinary.com/saimano/image/upload/v1622036153/RaidGuild/icons/red/consultations_lr6ef4.png",
-  "https://res.cloudinary.com/saimano/image/upload/v1622036154/RaidGuild/icons/red/designsprints__one_zuzzjt.png",
-  "https://res.cloudinary.com/saimano/image/upload/v1622036154/RaidGuild/icons/red/fullstackdev_pvshh4.png",
-  "https://res.cloudinary.com/saimano/image/upload/v1622036155/RaidGuild/icons/red/marketing_ge6ikg.png",
+  'https://res.cloudinary.com/saimano/image/upload/v1622036153/RaidGuild/icons/red/consultations_lr6ef4.png',
+  'https://res.cloudinary.com/saimano/image/upload/v1622036154/RaidGuild/icons/red/designsprints__one_zuzzjt.png',
+  'https://res.cloudinary.com/saimano/image/upload/v1622036154/RaidGuild/icons/red/fullstackdev_pvshh4.png',
+  'https://res.cloudinary.com/saimano/image/upload/v1622036155/RaidGuild/icons/red/marketing_ge6ikg.png',
 ];
 
 const cardProps = Array.from(new Array(4), (_, i) => ({
@@ -39,8 +34,15 @@ const cardProps = Array.from(new Array(4), (_, i) => ({
 }));
 
 const MultipleTemplate: Story<CardProps> = () => (
-  <Wrapper>
-    {cardProps.map((props) => <Card key={props.title} {...props} />)}
-  </Wrapper>
+  <SimpleGrid
+    columns={{ base: 1, md: 2, lg: 2 }}
+    gap={5}
+    my="2rem"
+    ml={{ lg: '3rem' }}
+  >
+    {cardProps.map((props) => (
+      <Card key={props.title} {...props} />
+    ))}
+  </SimpleGrid>
 );
 export const Multiple = MultipleTemplate.bind({});
